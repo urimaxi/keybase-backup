dee57ff31e86c74646b09b637e3241ae88dc11cd
Uri Maxima <uri@maxima.ninja>
2021-09-29 09:14:27 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require("fs-extra");
+
+const projectsRoot = "../_www"; // Path to all your projects
+const keybaseRoot = "../keybase"; // Path to Keybase directory
+
+const findKeybase = (folder) => {
+  let contents = fs.readdirSync(folder);
+  if (contents.includes(".keybase")) return `${folder}/.keybase`;
+  return contents
+    .map((path) => {
+      let newPath = `${folder}/${path}`;
+      if (!fs.lstatSync(newPath).isDirectory()) return null;
+      return findKeybase(newPath);
+    })
+    .flat()
+    .filter((e) => e);
+};
+
+const getIgnoredFiles = (file) => {
+  const constent = fs
+    .readFileSync(file, "utf-8")
+    .split("\n")
+    .filter((e) => e)
+    .filter((e) => e.charAt(0) !== "#")
+    .forEach((e) => {
+      let destination = `${keybaseRoot}${file.split(projectsRoot)[1]}`.replace(
+        "/.keybase",
+        `/${e}`
+      );
+      let source = file.replace("/.keybase", `/${e}`);
+      fs.copySync(source, destination);
+    });
+};
+
+findKeybase(projectsRoot).forEach((file) => getIgnoredFiles(file));
